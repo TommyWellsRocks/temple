d435abe93665d89ff48574053a3a819c0701851b
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-05-29 13:08:11 +0000

    Made workout specs interactive

--- src/scripts/workout/workout-overview.js
+++ src/scripts/workout/workout-overview.js
@@ -4,9 +4,29 @@ import { backButtonListener } from "../utils/backButtonListener.js";
 // Daily Quote
 function setDailyQuote(quote, author) {
 	document.querySelector(".quote-container").innerHTML = `
-    <p class="quote-container-header">Daily Quote</p>
-    <p class="quote">${quote}</p>
-    <p class="quote-author">- ${author}</p>
+        <p class="quote-container-header">Daily Quote</p>
+        <p class="quote">${quote}</p>
+        <p class="quote-author">- ${author}</p>
+    `;
+}
+
+function setWorkoutSpecs(timeMin, burnKcal) {
+	document.querySelector(".workout-specs-container").innerHTML = `
+        <div class="time-container">
+            <img src="../content/images/workout-pages/workout-overview/time.svg" class="specs-image" />
+            <div class="time-info">
+                <p class="specs-header">Time</p>
+                <p class="specs-text">${timeMin} min</p>
+            </div>
+            </div>
+            <div class="divider"></div>
+            <div class="burn-container">
+                <img src="../content/images/workout-pages/workout-overview/burn.svg" class="specs-image" />
+                <div class="burn-info">
+                    <p class="specs-header">Burn</p>
+                    <p class="specs-text">${burnKcal} kcal</p>
+                </div>
+        </div>
     `;
 }
 
@@ -16,3 +36,5 @@ backButtonListener(workoutOverviewURL);
 // Set Quote
 setDailyQuote("Slow Form + Big Weight = Big Muscles", "Tommy Wells");
 
+// Set Specs
+setWorkoutSpecs(60, 495);
