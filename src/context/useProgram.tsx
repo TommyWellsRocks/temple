bbc6cc55b27e518ee792d254a63c19f23ddd32b2
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-08-21 04:44:18 -0500

    Removed caching on useProgram (unstable_cache caused crash)

--- src/context/useProgram.tsx
+++ src/context/useProgram.tsx
@@ -1,14 +1,11 @@
 "use server";
 
-import { unstable_cache } from "next/cache";
-import { cache } from "react";
 import { getMyProgram } from "~/server/queries/workouts";
 
-export const useProgramClient = cache(
-  async (userId: string, programId: number) =>
-    await getMyProgram(userId, programId),
-);
-export const useProgram = unstable_cache(
-  async (userId: string, programId: number) =>
-    await getMyProgram(userId, programId),
-);
+export async function useProgramClient(userId: string, programId: number) {
+  return await getMyProgram(userId, programId)
+}
+
+export async function useProgram(userId: string, programId: number) {
+  return await getMyProgram(userId, programId)
+}
