f175229c11bb9867c5d7ddd0f9fa808f72928f5f
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-09-10 09:53:29 -0500

    Refactor program (days) page with HeaderNav

---
+++ src/app/workout/[programId]/_components/HeaderNav.tsx
@@ -0,0 +1,22 @@
+import { Navigation } from "~/components/ui/Navigation";
+import { EditButtonPopover } from "~/components/workout/EditButtonPopover";
+import { ProgramForm } from "~/components/workout/forms/ProgramForm";
+import { useProgram } from "~/hooks/workout/useProgram";
+
+export function HeaderNav() {
+  const program = useProgram((state) => state.program)!;
+
+  return (
+    <Navigation
+      backURL="/workout"
+      heading={program.name}
+      editButton={
+        <EditButtonPopover
+          title="Edit Program"
+          description={`Remember to click save when you're done.`}
+          formComponent={<ProgramForm programInfo={program} />}
+        />
+      }
+    />
+  );
+}
--- src/app/workout/[programId]/page.tsx
+++ src/app/workout/[programId]/page.tsx
@@ -1,40 +1,21 @@
 "use client";
 
-import { Navigation } from "~/components/ui/Navigation";
+import { HeaderNav } from "./_components/HeaderNav";
 import { Tabs } from "~/components/ui/tabs";
-import { EditButtonPopover } from "~/components/workout/EditButtonPopover";
 import { GroupDays, GroupsInfo } from "./_components/DayList";
-import { ProgramForm } from "~/components/workout/forms/ProgramForm";
 import { useProgram } from "~/hooks/workout/useProgram";
 
 // * DAYS PAGE
 
 export default function Days() {
-  const [programName, programGroups, userId] = useProgram((state) => [
-    state.program?.name,
-    state.programGroups,
-    state.program?.userId,
-  ]);
-  const program = useProgram((state) => state.program);
-  if (!programName || !programGroups || !userId) return;
+  const programGroups = useProgram((state) => state.programGroups);
+  if (!programGroups) return;
 
   const lastGroupId = String(programGroups[programGroups.length - 1]!.id);
 
   return (
     <>
-      <Navigation
-        backURL="/workout"
-        heading={programName}
-        editButton={
-          <EditButtonPopover
-            title="Edit Program"
-            description={`Remember to click save when you're done.`}
-            formComponent={
-              <ProgramForm userId={userId} programInfo={program!} />
-            }
-          />
-        }
-      />
+      <HeaderNav />
 
       <section>
         <Tabs defaultValue={lastGroupId}>
