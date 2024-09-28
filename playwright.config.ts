624114a4f99bd7fd0f9ac3b15e4b6c62c33bd2f5
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-09-28 10:16:26 -0500

    Playwright config 3 retries and 45 second timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,13 +12,13 @@ dotenv.config();
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  retries: 3,
+  timeout: 45 * 1000, // 45 Seconds
   testDir: "./src/__test__",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
