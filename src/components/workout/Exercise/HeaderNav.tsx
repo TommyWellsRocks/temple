bf78bad40c440e46f941f496e9d0914165d00588
Tommy Wells <155028305+NoReallyTommy@users.noreply.github.com>
2024-09-30 13:08:47 -0500

    Exercise Nav uses exercise edit button

--- src/components/workout/Exercise/HeaderNav.tsx
+++ src/components/workout/Exercise/HeaderNav.tsx
@@ -1,9 +1,8 @@
 import { useProgram } from "~/hooks/workout/useProgram/useProgram";
 
-import { Navigation } from "~/components/ui/Navigation";
-import { ExerciseForm } from "~/components/workout/forms/ExerciseForm/ExerciseForm";
-import { EditButtonPopover } from "~/components/workout/EditButtonPopover";
 import Loading from "~/app/loading";
+import { Navigation } from "~/components/ui/Navigation";
+import { EditButton } from "../EditExerciseButton";
 
 export function HeaderNav() {
   const dayExercise = useProgram((state) => state.dayExercise);
@@ -13,18 +12,7 @@ export function HeaderNav() {
     <Navigation
       backURL={`/workout/${dayExercise.programId}/${dayExercise.dayId}`}
       heading={`${dayExercise.notes?.name ? dayExercise.notes.name : dayExercise.info.name}`}
-      editButton={
-        <EditButtonPopover
-          title="Edit Exercise"
-          description={`Remember to click save when you're done.`}
-          formComponent={
-            <ExerciseForm
-              programId={dayExercise.programId}
-              dayExercise={dayExercise}
-            />
-          }
-        />
-      }
+      editButton={<EditButton exercise={dayExercise} />}
     />
   );
 }
